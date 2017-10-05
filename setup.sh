1c13732cc993b180a0d94988b1ea07f7fa49949d
Ubuntu <ubuntu@ip-10-0-0-14.ec2.internal>
2017-10-05 00:46:54 +0000

    added setup file

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#Include your credentials and elasticsearch IPs here
+#source file when done
+
+export AWS_ACCESS_KEY_ID=XXXXX
+export AWS_SECRET_ACCESS_KEY=XXXXX
+
+export ES_ACCESS_KEY_ID=XXXXX
+export ES_SECRET_ACCESS_KEY=XXXXX
+
+export MASTER_INTERNAL_IP=XXXXX
+export WORKER1_INTERNAL_IP=XXXXX
+export WORKER2_INTERNAL_IP=XXXXX
+export WORKER3_INTERNAL_IP=XXXXX
